512306c820a7332e02dd8ddeedba1a8236f6a001
jayji5204 <jayji5204@naver.com>
2024-10-17 00:16:26 +0900

    fix : 컨트롤러에서 게시글 작성, 페이지, 삭제 수정, 그에 따른 html 수정

--- board/board/src/main/java/com/example/board/controller/BoardController.java
+++ board/board/src/main/java/com/example/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.board.controller;
 import com.example.board.DTO.BoardRequestDTO;
 import com.example.board.DTO.BoardResponseDTO;
 import com.example.board.service.BoardService;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -28,30 +29,36 @@ public class BoardController {
     }
 
 
+
     // 게시글 작성 페이지
     @GetMapping("/create")
-    public String getCreatePage() {
+    public String getCreatePage(Model model) {
+        model.addAttribute("boardRequestDTO", new BoardRequestDTO());
         return "CreatePage";
     }
 
     @PostMapping("/create")
-    public String postCreatePage(@ModelAttribute BoardRequestDTO boardRequestDTO) {
+    public String postCreatePage(String title,String content) {
+        BoardRequestDTO boardRequestDTO = new BoardRequestDTO(title, content);
         boardService.createBoard(boardRequestDTO);
         return "redirect:/"; // 메인 페이지로 리다이렉트
     }
 
     // 게시글 페이지
-    @GetMapping("/{id}")
+    @GetMapping("/boards/{id}")
     public String getIdPage(@PathVariable Long id, Model model) {
-        Optional<BoardResponseDTO> boardResponseDTO = boardService.getBoard(id);
-        model.addAttribute("board", boardResponseDTO);
+        BoardResponseDTO boardResponseDTO = boardService.getBoard(id)
+                .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다.")); // Optional에서 값 꺼내기
+        model.addAttribute("board", boardResponseDTO); // DTO를 모델에 추가
         return "IdPage"; // 게시글 페이지로 이동
     }
+
 
     // 게시글 수정 페이지
     @GetMapping("/update/{id}")
     public String getUpdatePage(@PathVariable Long id, Model model) {
-        Optional<BoardResponseDTO> boardResponseDTO = boardService.getBoard(id);
+        BoardResponseDTO boardResponseDTO = boardService.getBoard(id)
+                .orElseThrow(() -> new EntityNotFoundException("게시글을 찾을 수 없습니다."));;
         model.addAttribute("board", boardResponseDTO);
         return "UpdatePage"; // 수정 페이지로 이동
     }
@@ -60,7 +67,7 @@ public class BoardController {
     @PostMapping("/update/{id}")
     public String postUpdatePage(@PathVariable Long id, @ModelAttribute BoardRequestDTO boardRequestDTO) {
         boardService.updateBoard(id, boardRequestDTO);
-        return "redirect:/{id}"; // 수정된 게시글 페이지로 리다이렉트
+        return "redirect:/boards/"+id; // 수정된 게시글 페이지로 리다이렉트
     }
 
     // 게시글 삭제
